eb9d22246111b86cb50be60e4de42f6c62e9321d
Mike Archie <rusty505@yahoo.com>
2017-05-23 13:13:11 -0700

    Switched activity layout to fragment, and all that entails, for assign 7

--- HM_ch09_ex4_NewsReader/app/build/generated/source/r/debug/com/murach/newsreader/R.java
+++ HM_ch09_ex4_NewsReader/app/build/generated/source/r/debug/com/murach/newsreader/R.java
@@ -18,16 +18,18 @@ public final class R {
     }
     public static final class id {
         public static final int descriptionTextView=0x7f080002;
-        public static final int itemsListView=0x7f080004;
+        public static final int itemsListView=0x7f080005;
+        public static final int items_fragment=0x7f080004;
         public static final int linkTextView=0x7f080003;
-        public static final int menu_settings=0x7f080005;
+        public static final int menu_settings=0x7f080006;
         public static final int pubDateTextView=0x7f080001;
         public static final int titleTextView=0x7f080000;
     }
     public static final class layout {
         public static final int activity_item=0x7f030000;
         public static final int activity_items=0x7f030001;
-        public static final int listview_item=0x7f030002;
+        public static final int fragment_items=0x7f030002;
+        public static final int listview_item=0x7f030003;
     }
     public static final class menu {
         public static final int activity_item_2=0x7f070000;
